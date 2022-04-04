e5a408a368160f10d3691f156d8357e3da87bf61
Aisa Nicevic <aisanicevic04@gmail.com>
2022-04-04 18:09:07 +0200

    js

---
+++ class-19/first.js
@@ -0,0 +1,28 @@
+// var a = 10 + "70";
+// var a = 10 - "70";
+// var a = 10 + "nesto";
+// console.log(typeof(a))
+// a = 20
+// console.log(a.toString());
+// console.log(typeof(a))
+// console.log(a)
+
+// var a = 10;
+// var b = 20;
+// var temp;
+// console.log(a, b);
+// temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+// var greeting = "Hello from the";
+// var name = "other side";
+// var fullGreating = greeting + name;
+// var age = 17
+// var fullGreating = `I am ${age} years old`
+// var greeting = "Helo I am" + age + "years old"
+// console.log(fullGreating)
+
+
+// undefined, null, "", naN, False ==> False values
