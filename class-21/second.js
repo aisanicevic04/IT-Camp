3bb96843624caafac8691f6f307982078f619885
Aisa Nicevic <aisanicevic04@gmail.com>
2022-04-07 17:53:03 +0200

    js

---
+++ class-21/second.js
@@ -0,0 +1,56 @@
+// var grade = 89;
+
+// if (grade > 55 && grade <=65) {
+//     console.log("ocena je 5")
+// }
+// else if (grade > 65 && grade <=75) {
+//     console.log("ocena je 6")
+// }
+// else if (grade > 75 && grade <=85) {
+//     console.log("ocena je 7")
+// }
+// else if (grade > 85 && grade <=95) {
+//     console.log("ocena je 8")
+// }
+// else if (grade > 95 && grade <=100) {
+//     console.log("ocena je 9")
+// }
+// else {
+//     console.log("niste polozili ispit")
+// }
+
+// var a = 20;
+// var b = 30;
+// var c = 55;
+// if (a > b) {
+//     console.log(`veci broj je ${a}`)
+// }
+// else {
+//     if (b > c) {
+//         console.log(`veci broj je ${b}`)
+//     } else {
+//         console.log(`veci broj je ${c}`)
+//     }
+// }
+//     if (c > a) {
+//         console.log(`veci broj je ${c}`)
+//     } else {
+//         console.log(`veci broj je ${a}`)
+//     }
+
+// var num1 = 20;
+// var num2 = 30;
+// var num3 = 55;
+
+// function compare(num1, num2, num3) {
+// 	if (num3 > num2 && num1 > num3) {
+// 		return num1 + ' is the biggest number';
+// 	} else if (num2 > num1 && num2 > num3) {
+// 		return num2 + ' is the biggest number';
+// 	} else {
+// 		return num3 + ' is the biggest number';
+// 	}
+// }
+// var results = compare(10, 20, 30);
+// console.log(results);
+
