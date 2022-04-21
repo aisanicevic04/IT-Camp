c6b5240772f6a667132cffa49552e119a77f9587
Aisa Nicevic <aisanicevic04@gmail.com>
2022-04-21 17:35:44 +0200

    count

---
+++ class-22/index.js
@@ -0,0 +1,55 @@
+// var sum = 0;
+// var n = 20;
+
+
+// for (let i = 1; index <= n; i++) {
+//     sum += i
+// }
+
+// function sum(n) {
+//     var s = 0;
+//     for (let i = 1; i <= n; i++) {
+//         s += i;
+//     }
+//     return s;
+// }
+
+// // console.log(sum(10))
+
+// function sumR(n) {
+//     var sum = 0;
+//     if (n === 1) {
+//         return 1;
+//     }
+//     sum = n + sumR(n - 1);
+//     return sum;
+// }
+
+// console.log(sumR(10))
+
+
+
+// console.log(sum(10))
+
+// function sumR(n) {
+//     var sum = 1;
+//     if (n === 1) {
+//         return 1;
+//     }
+//     sum = n * sumR(n - 1);
+//     return sum;
+// }
+
+// console.log(sumR(4))
+
+
+function countdown(n){
+  if (n === 0) {
+    return [];
+  } else {
+    var arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+console.log(countdown(5));
