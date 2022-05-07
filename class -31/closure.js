a578f54a79869bd9be96bbfa7c5656f47d3d9341
Aisa Nicevic <aisanicevic04@gmail.com>
2022-05-07 18:01:20 +0200

    closure

--- class -31/closure.js
+++ class -31/closure.js
@@ -1,18 +1,33 @@
 // let name = 'John';
 
-// function greeting() { 
+// function greeting() {
 //     let message = 'Hi';
 //     console.log(message + ' '+ name);
 // }
 
-function greeting() {
-    let message = 'Hi';
+// function greeting() {
+//     let message = 'Hi';
 
-    function sayHi() {
-        console.log(message);
+//     function sayHi() {
+//         console.log(message);
+//     }
+
+//     sayHi();
+// }
+
+// greeting();
+
+
+const closure = (array) =>{
+    return () => {
+        return array.length>0 ? true:false
     }
-
-    sayHi();
 }
 
-greeting();
+const array1 = [223, 44, 3, 3, 22, 22]
+const array2 = [12]
+
+const isEmpty1 = closure(array1)
+const isEmpty2 = closure(array2)
+
+console.log()
