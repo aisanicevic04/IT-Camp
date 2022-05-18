25bfc7128dcb9890a60363bfd1b2718d40672f5b
Aisa Nicevic <aisanicevic04@gmail.com>
2022-05-18 20:51:00 +0200

    js

---
+++ class-32/index.js
@@ -0,0 +1,42 @@
+// random = Math.round(Math.random() * 100);
+
+// unos = parseInt(prompt("unesite broj"));
+// while (unos !== random) {
+//     console.log("pogresan broj!")
+//     if (unos > random) {
+//         unos.parseInt(prompt("pokusajte opet"))
+//     }
+//     else {
+//         unos.parseInt(prompt("broj je netacan"));
+//     }
+
+// }
+
+// const number = parseInt(prompt("Enter a number: "));
+// while (number > 0) {
+//     if (number > 0) {
+//         console.log("The number is positive");
+//     }
+
+//     else if (number == 0) {
+//         console.log("The number is zero");
+//     }
+
+//     else {
+//         console.log("The number is negative");
+//     }
+// }
+
+
+
+// unos = prompt("unesite ime")
+
+// kod = "END"
+
+// while (unos !== kod) {
+//     console.log(unos)
+//     unos = prompt("unesite ime")
+
+// }
+
+
