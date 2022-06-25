5b924a43393dfd04c03903a9f03030ba4e001603
Aisa Nicevic <aisanicevic04@gmail.com>
2022-06-25 19:30:39 +0200

    class

--- javasc/function.js
+++ javasc/function.js
@@ -137,15 +137,30 @@
 
 
 
-console.log(1)
-console.log(2)
+// console.log(1)
+// console.log(2)
 
-setTimeout(() => {
-    console.log(3);
-}, 1000);
+// setTimeout(() => {
+//     console.log(3);
+// }, 1000);
 
-console.log(4)
+// console.log(4)
 
-setTimeout(() => {
-    console.log(5);
-}, 1000);
+// setTimeout(() => {
+//     console.log(5);
+// }, 1000);
+
+
+const greet = () => {
+    console.log("Hi")
+}
+
+const showAlert = () => {
+    console.log("Alert danger")
+}
+
+setTimeout((showAlert) => {
+    
+}, 2000);
+
+greet()
