36d428b2790c5a4a9bbf851a73568f3c318e09d4
Aisa Nicevic <aisanicevic04@gmail.com>
2022-07-01 23:39:36 +0200

    cpmmit

---
+++ src/functional/Card.jsx
@@ -0,0 +1,12 @@
+import "./Card.css";
+const Card = (props) => {
+  const { url, title, job } = props;
+
+  return (
+    <div className="card">
+      <img src={url} alt="Nesto" />
+      <p className="title">{title}</p>
+    </div>
+  );
+};
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Card from "./functional/Card";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <Card
+        title="TWITTER"
+        job=""
+        url="https://play-lh.googleusercontent.com/x3XxTcEYG6hYRZwnWAUfMavRfNNBl8OZweUgZDf2jUJ3qjg2p91Y8MudeXumaQLily0"
+      />
+      <Card
+        title="INSTAGRAM"
+        job=""
+        url="https://blog.publer.io/wp-content/uploads/2022/03/alexander-shatov-71Qk8ODIBko-unsplash.jpg"
+      />
+      <Card
+        title="FACEBOOK"
+        job=""
+        url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+      />
+    </div>
   </React.StrictMode>
 );
 
